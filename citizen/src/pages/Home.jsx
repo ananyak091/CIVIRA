507bb161bc954fdeb27332299383504f2e73479c
Ananya Kundu <ananyak091@gmail.com>
2025-12-13 00:37:24 +0530

    Home Component file set up completed

--- citizen/src/pages/Home.jsx
+++ citizen/src/pages/Home.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { Hero } from "../components/HomeComponent/Hero";
+import { HowItWork } from "../components/HomeComponent/HowItWork";
+import { Features } from "../components/HomeComponent/Features";
+import { Category } from "../components/HomeComponent/Category";
+import { CTA } from "../components/HomeComponent/CTA";
+import { Testimonial } from "../components/HomeComponent/Testimonial";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Hero />
+      <HowItWork />
+      <Features />
+      <Category />
+      <CTA />
+      <Testimonial />
+    </>
+  );
 };
